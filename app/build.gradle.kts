e9d9fd1a288a785fc49eb0fd761eb35db047b8e7
ZEPHA <zepha8@gmail.com>
2023-11-14 15:31:02 +0000

    here we go

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,5 @@ dependencies {
 
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+    implementation("androidx.autofill:autofill:1.1.0")
 }
